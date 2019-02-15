d914da25b0b494e5c7f8263c1f958bafea9e3d9a
Guy Peled <guy.p@taboola.com>
2019-02-15 16:41:43 +0200

    Initial concepts.

--- example/main.dart
+++ example/main.dart
@@ -1,21 +1,39 @@
 import 'package:redux_saga/redux_saga.dart';
+import 'dart:io';
 
-main() {
+main() async {
 
 
   SagaManager manager = SagaManager(test());
 
-  print("run:${manager.run()}");
+
+  print("run");
+  while(true) {
+    RunnableStatus status = manager.run();
+
+    print("run:${status}");
+
+    if(status != RunnableStatus.Done) {
+      await Future.delayed(Duration(seconds: 1));
+    } else {
+      break;
+    }
+  }
+
+  print("Done.");
 
 }
 
 Iterable<Runnable> test() sync* {
-  yield fork([error1(test1()), test2()]);
+  print("in test");
+  yield all([error1(test1()), test2()]);
+  print("out test");
 }
 
 
 Iterable<Runnable> error1(Iterable<Runnable> saga) sync* {
 
+  print("in error1");
   try {
     yield put("before code");
     yield* saga;
@@ -24,23 +42,33 @@ Iterable<Runnable> error1(Iterable<Runnable> saga) sync* {
   catch(e) {
     yield put(e);
   }
+
+  print("out error1");
 }
 
 Iterable<Runnable> test1() sync* {
 
-  try {
-    String action;
-    yield take("test", (v) => action = v);
-    yield put(action);
-  } catch(e) {
-    yield put(e);
-  }
+  print("in test1");
+  //while(true) {
+    try {
+      String action;
+      yield take("test", (v) => action = v);
+      yield put(action);
+    } catch (e) {
+      yield put(e);
+    }
+  //}
+
+  print("out test1");
 }
 
 Iterable<Runnable> test2() sync* {
 
+  print("in test2");
   yield take("test");
-  yield put("dd");
+  for(int i=0;i<4;i++) {
+    yield put("iterator:${i}");
+  }
   String v;
   yield call(getSomething(), (value) => v = value);
   yield put(v);
@@ -50,6 +78,7 @@ Iterable<Runnable> test2() sync* {
   if(ss == null) {
     yield put(ss);
   }
+  print("out test2");
 }
 
 Future<String> getSomething() {
