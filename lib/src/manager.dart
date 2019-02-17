5787a0fc3987142a782b290239e0d6f6250833aa
Guy Peled <guy.p@taboola.com>
2019-02-17 09:21:53 +0200

    Initial concepts.

--- lib/src/manager.dart
+++ lib/src/manager.dart
@@ -1,3 +1,72 @@
+import 'dart:async';
+import 'process.dart';
+import 'package:built_redux/built_redux.dart';
+import 'package:redux_saga/redux_saga.dart';
+import 'package:built_value/built_value.dart';
 
+class SagaMiddlewareManager<
+StateType extends Built<StateType, StateBuilderType>,
+StateBuilderType extends Builder<StateType, StateBuilderType>,
+ActionsType extends ReduxActions> {
+  final ProcessTask _process;
 
+  final StreamController<Object> _observable = new StreamController();
 
+  bool _initialized = false;
+
+  Stream<Object> _actions;
+
+  SagaMiddlewareManager(List<Iterable<Runnable>> runnableList)
+      : _process = ProcessTask(Runnable.createTasksFromList(runnableList)) {
+    _actions = _observable.stream.asBroadcastStream();
+  }
+
+  void init(MiddlewareApi<StateType, StateBuilderType, ActionsType> api) {
+    if(!_initialized) {
+      _initialized = true;
+      this._process.run(this);
+    }
+  }
+
+  RunnableStatus run() {
+    return this._process.run(this);
+  }
+
+  ActionHandler next(ActionHandler next) {
+    return (Action<dynamic> action) {
+
+      print("sinked:${action}");
+
+      _observable.sink.add(action);
+
+      if (next != null) {
+        next(action);
+      }
+    };
+  }
+
+  RunnableStatus get status {
+    return this._process.status;
+  }
+
+  void put<PayloadType>(ActionName<PayloadType> actionName, PayloadType payload) {
+    Action<PayloadType>(actionName.name, payload);
+  }
+
+  ValueType select<SelectorType, ValueType>(SelectorType selector) {
+    print("selecting:${selector}");
+    return selector as ValueType;
+  }
+
+  Future<Action<PayloadType>> take<PayloadType>(
+      ActionName<PayloadType> actionName) async {
+    if (actionName != null) {
+      return _actions
+          .where((action) => action is Action<PayloadType>)
+          .map((action) => action as Action<PayloadType>)
+          .where((action) => action.name == actionName.name)
+          .first;
+    }
+    return null;
+  }
+}
--- lib/src/middleware.dart
+++ lib/src/middleware.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'process.dart';
 import 'package:built_redux/built_redux.dart';
 import 'package:redux_saga/redux_saga.dart';
 import 'package:built_value/built_value.dart';
@@ -8,65 +6,12 @@ Middleware<StateType, StateBuilderType, ActionsType> createSagaMiddleware<
     StateType extends Built<StateType, StateBuilderType>,
     StateBuilderType extends Builder<StateType, StateBuilderType>,
     ActionsType extends ReduxActions>(List<Iterable<Runnable>> runnableList) {
-  SagaMiddlewareManager manager = new SagaMiddlewareManager(runnableList);
+  SagaMiddlewareManager<StateType, StateBuilderType, ActionsType> manager = new SagaMiddlewareManager(runnableList);
   manager.run();
 
   return (MiddlewareApi<StateType, StateBuilderType, ActionsType> api) {
+    manager.init(api);
+
     return manager.next;
   };
-}
-
-class SagaMiddlewareManager {
-  final ProcessTask _process;
-
-  final StreamController<Object> _observable = new StreamController();
-
-  Stream<Object> _actions;
-
-  SagaMiddlewareManager(List<Iterable<Runnable>> runnableList)
-      : _process = ProcessTask(Runnable.createTasksFromList(runnableList)) {
-    _actions = _observable.stream.asBroadcastStream();
-  }
-
-  RunnableStatus run() {
-    return this._process.run(this);
-  }
-
-  ActionHandler next(ActionHandler next) {
-    return (Action<dynamic> action) {
-
-      print("sinked:${action}");
-
-      _observable.sink.add(action);
-
-      if (next != null) {
-        next(action);
-      }
-    };
-  }
-
-  RunnableStatus get status {
-    return this._process.status;
-  }
-
-  void put<PayloadType>(ActionName<PayloadType> actionName, PayloadType payload) {
-    Action<PayloadType>(actionName.name, payload);
-  }
-
-  ValueType select<SelectorType, ValueType>(SelectorType selector) {
-    print("selecting:${selector}");
-    return selector as ValueType;
-  }
-
-  Future<Action<PayloadType>> take<PayloadType>(
-      ActionName<PayloadType> actionName) async {
-    if (actionName != null) {
-      return _actions
-          .where((action) => action is Action<PayloadType>)
-          .map((action) => action as Action<PayloadType>)
-          .where((action) => action.name == actionName.name)
-          .first;
-    }
-    return null;
-  }
 }
