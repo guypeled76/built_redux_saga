69c8fee5ad02894cd29d8d1d83a60aca6765fafd
Guy Peled <guy.p@taboola.com>
2019-07-07 23:02:28 +0300

    version

--- test/test.dart
+++ test/test.dart
@@ -41,5 +41,67 @@ void main() {
       _store.actions.increment(3);
       assert(_store.state.count == 7, "Count should be 7.");
     });
+
+    test('Select/Register sanity test', () {
+
+      Iterable<Runnable> registerSaga() sync* {
+        yield register("test");
+
+        String value;
+        yield select<String>((result) {
+          value = result;
+        });
+
+        if(value == "test") {
+          yield put(AppActionsNames.increment, 10);
+        }
+      }
+
+      Store<AppState, AppStateBuilder, AppActions> _store = createAppState([registerSaga()]);
+
+
+      assert(_store.state.count == 10, "Count should be 10");
+    });
+
+
+    test('Fork sanity test', () {
+
+      Iterable<Runnable> forkedProcess() sync* {
+        while(true) {
+          yield take(AppActionsNames.increment);
+          yield put(AppActionsNames.increment, 3);
+        }
+      }
+
+      Iterable<Runnable> testFork() sync* {
+
+        yield fork(forkedProcess());
+
+        while(true) {
+          yield take(AppActionsNames.log);
+          yield put(AppActionsNames.increment, 1);
+        }
+
+      }
+
+      Store<AppState, AppStateBuilder, AppActions> _store = createAppState([testFork()]);
+
+      _store.actions.increment(3);
+
+      assert(_store.state.count == 6, "Count should be 6");
+
+      _store.actions.log("test");
+
+      assert(_store.state.count == 7, "Count should be 7");
+
+      _store.actions.increment(3);
+
+      assert(_store.state.count == 13, "Count should be 6");
+
+      _store.actions.log("test");
+
+      assert(_store.state.count == 14, "Count should be 7");
+
+    });
   });
 }
